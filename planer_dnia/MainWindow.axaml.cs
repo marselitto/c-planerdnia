568c29eba1be03628a19ae3f032a5bad10b9dc1a
szybek <jakub.jaworowski@cyfroweszkoly.pl>
2025-03-18 15:01:19 +0100

    Third commit

--- planer_dnia/MainWindow.axaml.cs
+++ planer_dnia/MainWindow.axaml.cs
@@ -1,11 +1,20 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace planer_dnia;
 
 public partial class MainWindow : Window
 {
+    private TextBox _inputTextBox;
+    private Button _button;
+    
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_Click(object? sender, RoutedEventArgs e)
+    {
+        
     }
 }
